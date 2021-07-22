3e7067e21a1151bea63ad6ef370b80b35a28d304
Lucas DevOps <51280960+LucasDevolps@users.noreply.github.com>
2021-07-21 22:22:14 -0300

    Lendo dados

---
+++ CursoCSharp/Fundamentos/FormatandoNumero.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class FormatandoNumero
+    {
+        public static void Executar()
+        {
+
+        }
+    }
+}
