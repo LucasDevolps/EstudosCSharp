cd064179c342616be3da46658ad41a5ca8d93597
Lucas DevOps <51280960+LucasDevolps@users.noreply.github.com>
2021-07-21 23:12:02 -0300

    Conversoes

---
+++ CursoCSharp/Fundamentos/Conversoes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class Conversoes
+    {
+        public static void Executar()
+        {
+            int inteiro = 10;
+            double quebrado = inteiro;
+            Console.WriteLine(quebrado);
+
+            double nota = 9.7;
+            //Cast, conversão explicita
+            int notaTruncada = (int) nota;
+
+            Console.WriteLine("Nota truncada: {0}", notaTruncada);
+
+            Console.Write("Digite sua idade: ");
+            string idadeString = Console.ReadLine();
+
+            int IdadeInteiro = int.Parse(idadeString);
+            Console.WriteLine("Idade inserida: {0}", idadeString);
+
+            IdadeInteiro = Convert.ToInt32(idadeString);
+            Console.WriteLine("Resultado: {0}", idadeString);
+
+            /*
+            Console.Write("Digite um número: ");
+            string palavra = Console.ReadLine();
+            //int numero;
+            //int.TryParse(palavra, out int numero);
+            Console.WriteLine("Resultado: {0}", numero);
+
+            */
+
+            Console.Write("Digite o primeiro número: ");
+            int.TryParse(Console.ReadLine(), out int numero);
+            Console.WriteLine("Resultado1: {0}", numero);
+
+            Console.Write("Digite o segundo número: ");
+            int.TryParse(Console.ReadLine(), out int numero2);
+            Console.WriteLine("Resultado2: {0}", numero2);
+
+
+        }
+    }
+}
--- CursoCSharp/Fundamentos/FormatandoNumero.cs
+++ CursoCSharp/Fundamentos/FormatandoNumero.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Globalization;
 
 namespace CursoCSharp.Fundamentos
 {
@@ -8,7 +7,29 @@ namespace CursoCSharp.Fundamentos
     {
         public static void Executar()
         {
+            double valor = 15.175;
 
+            //Uma casa decimal
+            Console.WriteLine(valor.ToString("F1"));
+
+            //Transformando em moeda
+            Console.WriteLine(valor.ToString("C"));
+
+            //Percentual
+            Console.WriteLine(valor.ToString("P"));
+
+            //Um numero com duas casas decimais 
+            Console.WriteLine(valor.ToString("#.##"));
+
+            CultureInfo cultura = new CultureInfo("pt-BR");
+
+            //Valor de moeda sem casas decimais 
+            Console.WriteLine(valor.ToString("C0",cultura));
+
+            int inteiro = 256;
+
+            //Numero com 10 posições 
+            Console.WriteLine(inteiro.ToString("D10"));
         }
     }
 }
