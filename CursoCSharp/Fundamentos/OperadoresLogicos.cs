aef61ec6cdfa469d88d04fbf964d8b3381cc6a92
Lucas DevOps <51280960+LucasDevolps@users.noreply.github.com>
2021-07-27 14:09:24 -0300

    Atualizando o primeiro capitulo dos estudos

---
+++ CursoCSharp/Fundamentos/OperadoresLogicos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class OperadoresLogicos
+    {
+        public static void Executar()
+        {
+            var executouTrabalho1 = false;
+            var executouTrabalho2 = false;
+
+            bool comrouTv50 = executouTrabalho1 && executouTrabalho2;
+            Console.WriteLine("Comprou a Tv 50? {0}", comrouTv50);
+
+            var comprouSorvete = executouTrabalho1 || executouTrabalho2;
+            Console.WriteLine("Comprou o soevete? {0}", comprouSorvete);
+
+            var comprouTv32 = executouTrabalho1 ^ executouTrabalho2;
+            Console.WriteLine("Comprou a Tv 32? {0}", comprouTv32);
+
+            Console.WriteLine("Mais saudável? {0}", !comprouSorvete);
+        }
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -16,6 +16,12 @@ namespace CursoCSharp {
                 {"Lendo Dados  - Fundamentos", LendoDados.Executar},
                 {"Formatando numeros  - Fundamentos", FormatandoNumero.Executar},
                 {"Conversões - Fundamentos", Conversoes.Executar},
+                {"Operadores Aritmeticos - Fundamentos", OperadoresAritmeticos.Executar},
+                {"Operadores Relacionais - Fundamentos", OperadoresRelacionais.Executar},
+                {"Operadores Lógicos - Fundamentos", OperadoresLogicos.Executar},
+                {"Operadores de Atribuição - Fundamentos", OperadoresAtribuicao.Executar},
+                {"Operadores Unários - Fundamentos", OperadoresUnarios.Executar},
+                {"Operador Ternário - Fundamentos", OperadorTernario.Executar},
             });
 
             central.SelecionarEExecutar();
