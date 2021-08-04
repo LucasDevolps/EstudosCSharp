06fece7219a847aeba10f7970d3f2860612c5205
Lucas DevOps <51280960+LucasDevolps@users.noreply.github.com>
2021-08-04 11:40:15 -0300

    Props

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -53,7 +53,9 @@ namespace CursoCSharp {
                 {"Atributos Estáticos - Classes e Métodos", AtributosEstaticos.Executar },
                 {"Desafio Atributo - Classes e Métodos", DesafioAtributo.Executar},
                 {"Params - Classes e Métodos",Params.Executar },
-                {"Parametros Nomeados - Classes e Métodos", ParametrosNomeados.Executar }
+                {"Parametros Nomeados - Classes e Métodos", ParametrosNomeados.Executar },
+                {"Get Set - Classes e Métodos",  GetSet.Executar},
+                {"Props - Classes e Métodos", Props.Executar }
 
             });
 
