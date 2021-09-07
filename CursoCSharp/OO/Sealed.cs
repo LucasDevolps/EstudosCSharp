6c19f02497346126e29817618a0a4ea7687c7a85
Lucas DevOps <51280960+LucasDevolps@users.noreply.github.com>
2021-09-07 12:43:42 -0300

    Conceito Sealed

--- CursoCSharp/OO/Sealed.cs
+++ CursoCSharp/OO/Sealed.cs
@@ -5,11 +5,44 @@ using System.Text;
 
 namespace CursoCSharp.OO
 {
+    sealed class SemFilho
+    {
+        public double ValorDaFortuna()
+        {
+            return 1_407_033.65;
+        }
+    }
+
+    class Avo
+    {
+        public virtual bool HonrarNomeFamilia()
+        {
+            return true;
+        }
+    }
+
+    class Pai : Avo
+    {
+        public sealed override bool HonrarNomeFamilia()
+        {
+            return true;
+        }
+    }
+
+    class FilhoRebelde : Pai
+    {
+        
+    }
+
     class Sealed
     {
         public static void Executar()
         {
+            SemFilho semFilho = new SemFilho();
+            Console.WriteLine(semFilho.ValorDaFortuna());
 
+            FilhoRebelde filho = new FilhoRebelde();
+            Console.WriteLine(filho.HonrarNomeFamilia());
         }
     }
 }
