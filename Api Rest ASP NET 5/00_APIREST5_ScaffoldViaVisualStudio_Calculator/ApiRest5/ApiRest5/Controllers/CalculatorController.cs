7c837b79b6cbf0245c1f17cc5cb9868c80ba190b
Lucas DevOps <51280960+LucasDevolps@users.noreply.github.com>
2021-11-24 21:54:48 -0300

    Implementando calculadora

---
+++ Api Rest ASP NET 5/00_APIREST5_ScaffoldViaVisualStudio_Calculator/ApiRest5/ApiRest5/Controllers/CalculatorController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRest5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+        /*
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            var Url = HttpContext.Request.Query;            
+
+            if (Isnumric(firstNumber) && Isnumric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+        */
+
+        /*
+            DESAFIO!!!
+        
+            IMPLEMENTAR AS DEMIAS OPERAÇÕES
+            
+            * Subtração
+            * Multiplicação
+            * Divisão
+            * Média
+            * Raiz Quadrada
+        */
+
+        [HttpGet("{operation}/{firstNumber}/{secondNumber}")]
+        public IActionResult Calc(string calc,string firstNumber, string secondNumber)
+        {
+            calc = HttpContext.Request.Path.Value.ToString();
+
+            if (calc.Contains("sub"))
+            {
+                var resultado = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(resultado.ToString());
+            }
+            else if (calc.Contains("mul"))
+            {
+                var resultado = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(resultado.ToString());
+            }
+            else if (calc.Contains("div"))
+            {
+                var resultado = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(resultado.ToString());
+            }
+            else if (calc.Contains("sum"))
+            {
+                var resultado = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(resultado.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+        
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+            if(decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool Isnumric(string strNumber)
+        {
+            double number;
+            bool isNumer = double.TryParse(strNumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out number);
+            return isNumer;
+        }
+    }
+}
